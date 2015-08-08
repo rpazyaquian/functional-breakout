683b0bcde45fa1d0133a17c82af332b824dd4a14
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-08 13:12:41 -0400

    expanding initialization

--- src/game_engine/core.clj
+++ src/game_engine/core.clj
@@ -4,9 +4,29 @@
             [quil.core :as q]
             [quil.middleware :as m]))
 
-(def initial-state {:player {:x 0
-                             :y 0
-                             :jumping true}})
+(def map-height 512)
+(def map-width 256)
+
+(def screen-height 640)
+(def screen-width 360)
+
+(defn make-paddle [map-height map-width paddle-height paddle-width]
+  (let [x (center map-width paddle-width)
+        y (margin map-height paddle-height margin)]
+    {:x x
+     :y y
+     :width paddle-width
+     :height paddle-height}))
+
+; what IS my initial state?
+; i have one or more paddles,
+; i have one or more balls,
+; i have one or more bricks.
+(def initial-state
+  (let [initial-paddle (make-paddle map-height map-width 64 16)])
+  {:paddles [initial-paddle]
+   :ball [initial-ball]
+   :bricks initial-bricks})
 
 (def inputs [:a :right])
 
@@ -15,3 +35,8 @@
   [& args]
   (let [game-engine game-loop/game-engine]
     (game-engine initial-state inputs)))
+
+; how's rendering gonna be handled?
+; are we gonna block it?
+; i think the best thing to do is to create a sketch
+; and have it take stuff off the queue.
