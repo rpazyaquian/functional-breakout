b769cf8715af390c1640e4582606f170704acc95
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-31 16:28:17 -0400

    define initial components, entities

--- src/game_engine/logic.clj
+++ src/game_engine/logic.clj
@@ -1,6 +1,60 @@
 (ns game-engine.logic
   (:gen-class))
 
+;; Define samples of our components and entities.
+
+(def position-component
+  {:entity "entity"
+   :x 0
+   :y 0})
+
+(def velocity-component
+  {:entity "entity"
+   :vx 0
+   :vy 0})
+
+(def rectangle-size-component
+  {:entity "entity"
+   :w 40
+   :h 40})
+
+(def circle-size-component
+  {:entity "entity"
+   :d 10})
+
+;; Define our initial entities and components.
+
+(def initial-entities
+  {:entities ["paddle"
+              "ball"
+              "brick"]})
+
+(def initial-components
+  ;; key concept: **struct of arrays**
+  {:position-components [{:entity "paddle"
+                          :x 0
+                          :y 0}
+                         {:entity "ball"
+                          :x 0
+                          :y 0}
+                         {:entity "brick"
+                          :x 0
+                          :y 0}]
+   :velocity-components [{:entity "paddle"
+                          :vx 0
+                          :vy 0}
+                         {:entity "ball"
+                          :vx 0
+                          :vy 0}]
+   :rectangle-size-components [{:entity "paddle"
+                                :w 40
+                                :h 10}
+                               {:entity "brick"
+                                :w 40
+                                :h 10}]
+   :circle-size-components [{:entity "ball"
+                             :d 10}]})
+
 (defn new-game-state []
   {:paddles [{:x 320
               :y 100
