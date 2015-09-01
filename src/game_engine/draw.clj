7e12a787ea2b9e96535a1b3643f66887b66ff6ac
Rebecca Paz <r.pazyaquian@gmail.com>
2015-09-01 14:29:52 -0400

    comment out draw for now

--- src/game_engine/draw.clj
+++ src/game_engine/draw.clj
@@ -2,39 +2,42 @@
   (:gen-class)
   (:require [quil.core :as q]))
 
-(defn filter-paddles [state]
-  (let [{:keys [position-components rectangle-size-components]} state
-        position-component (filter #(= (:entity %) "paddle") position-components)
-        rectangle-size-component (filter #(= (:entity %) "paddle") rectangle-size-components)]
-    (merge position-component rectangle-size-component)))
+; (defn filter-paddles [state]
+;   (let [{:keys [position-components rectangle-size-components]} state
+;         position-component (filter #(= (:entity %) "paddle") position-components)
+;         rectangle-size-component (filter #(= (:entity %) "paddle") rectangle-size-components)]
+;     (merge position-component rectangle-size-component)))
 
-(defn filter-balls [state])
+; (defn filter-balls [state])
 
-(defn filter-bricks [state])
+; (defn filter-bricks [state])
 
-(defn render-filter [state]
-  {:paddles (filter-paddles state)
-   :balls (filter-balls balls)
-   :bricks (filter-bricks bricks)})
+; (defn render-filter [state]
+;   {:paddles (filter-paddles state)
+;    :balls (filter-balls balls)
+;    :bricks (filter-bricks bricks)})
 
-(defn draw-paddle [paddle]
-  ; expecting a "paddle" to be a map like: {:x 0 :y 0 :w 0 :h 0}
-  (let [{:keys [x y w h]} paddle]
-    (q/fill 0)
-    (q/rect x y w h)))
+; (defn draw-paddle [paddle]
+;   ; expecting a "paddle" to be a map like: {:x 0 :y 0 :w 0 :h 0}
+;   (let [{:keys [x y w h]} paddle]
+;     (q/fill 0)
+;     (q/rect x y w h)))
 
-(defn draw-ball [ball]
-  (let [{:keys [x y d]} ball]
-    (q/fill 0)
-    (q/ellipse x y d d)))
+; (defn draw-ball [ball]
+;   (let [{:keys [x y d]} ball]
+;     (q/fill 0)
+;     (q/ellipse x y d d)))
 
-(defn draw-brick [brick]
-  (let [{:keys [x y w h]} brick]
-    (q/fill 0)
-    (q/rect x y w h)))
+; (defn draw-brick [brick]
+;   (let [{:keys [x y w h]} brick]
+;     (q/fill 0)
+;     (q/rect x y w h)))
 
-(defn draw [state]
-  (let [{:keys [paddles balls bricks]} (render-filter state)]
-    (dorun (map draw-paddle paddles))
-    (dorun (map draw-ball balls))
-    (dorun (map draw-brick bricks))))
+; (defn draw-state [state]
+;   (let [{:keys [paddles balls bricks]} (render-filter state)]
+;     (dorun (map draw-paddle paddles))
+;     (dorun (map draw-ball balls))
+;     (dorun (map draw-brick bricks))))
+
+(defn draw-state [state]
+  state)
